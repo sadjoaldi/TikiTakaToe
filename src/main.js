71416d36ce5366b7ba5132868b8c235ee0cdc02a
sadjoaldi <alsannealdi@hotmail.com>
2024-05-22 14:30:47 +0200

    js over

--- src/main.js
+++ src/main.js
@@ -19,8 +19,8 @@ const restartBtn = document.querySelector("#btnRestart");
 let isPlayerTurn = false;
 
 restartBtn.addEventListener("click", startGame);
-
-startGame = () => {
+// function pour lancer le jeu
+function startGame() {
   isPlayerTurn = false;
   cellData.forEach((cell) => {
     cell.classList.remove(PLAYER_O_CLASS);
@@ -30,10 +30,12 @@ startGame = () => {
   });
   setBoardHoverClass();
   winningMessage.classList.remove("show");
-};
+}
 
-const handleCellClick = (e) => {
+// event qui lance le jeu au clic
+function handleCellClick(e) {
   const cell = e.target;
+  console.log(cell);
   const currentClass = isPlayerTurn ? PLAYER_X_CLASS : PLAYER_O_CLASS;
   placeMark(cell, currentClass);
   if (checkWin(currentClass)) {
@@ -44,4 +46,54 @@ const handleCellClick = (e) => {
     swapTurns();
     setBoardHoverClass();
   }
-};
+}
+
+// funciton qui met fin au jeu
+
+function endGame(draw) {
+  if (draw) {
+    winningMessageText.innerHTML = "It's a draw";
+  } else {
+    winningMessageText.innerHTML = `Player wiht ${
+      isPlayerTurn ? "O's" : "X's"
+    } win!`;
+  }
+
+  winningMessage.classList.add("show");
+}
+
+function isDraw() {
+  return [...cellData].every((cell) => {
+    return (
+      cell.classList.contains(PLAYER_X_CLASS) ||
+      cell.classList.contains(PLAYER_O_CLASS)
+    );
+  });
+}
+
+// function qui permet de mettre le caractère dans la cellule cible
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass);
+}
+
+// function qui permet l'alternance des tours pour chaque joueur lorsque le caractère est mit par un joeur pour passer au joueur suivant
+
+function swapTurns() {
+  isPlayerTurn = !isPlayerTurn;
+}
+
+//
+function setBoardHoverClass() {
+  board.classList.remove(PLAYER_X_CLASS);
+  board.classList.remove(PLAYER_O_CLASS);
+  isPlayerTurn
+    ? board.classList.add(PLAYER_O_CLASS)
+    : board.classList.add(PLAYER_X_CLASS);
+}
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(currentClass);
+    });
+  });
+}
